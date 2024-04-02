25fd66263727bbffd154f2fdebd7f99475413ea7
arpan <arpanbareja@gmail.com>
2024-04-02 13:24:59 +0530

    first commit

---
+++ client/script.js
@@ -0,0 +1,162 @@
+const socket = io();
+let myTurn = true,
+  symbol , clicks = 0 ;
+let no_draw = true;
+let buttonSel = document.querySelectorAll(".box");
+let messageSel = document.querySelector("#message");
+let loaderSel = document.querySelector("#loader");
+let gameRegionSel = document.querySelector(".outer");
+let resetSel = document.querySelector("#reset");
+
+// disable button function
+// true for disable , false for enable
+function disableEnableBtn(val) {
+  buttonSel.forEach((button) => {
+    button.disabled = val;
+  });
+}
+
+//Binding buttons on board
+document.addEventListener("DOMContentLoaded", () => {
+  //disbled all buttons and added an event listener to them
+  buttonSel.forEach((button) => {
+    button.disabled = true;
+    button.addEventListener("click", makeMove);
+  });
+});
+
+// whenever a pair joins the game
+socket.on("game.begin", (data) => {
+  loaderSel.style.display = "none";
+  messageSel.style.height = "20vh";
+  messageSel.style.fontSize = "20px";
+  gameRegionSel.style.display = "flex";
+  symbol = data.symbol;
+  myTurn = symbol === "X";
+  displayMessage();
+});
+
+// display message as per user/opponent turn's and disables, enables button
+function displayMessage() {
+  if (!myTurn) {
+    messageSel.textContent = "Your Opponent's Turn";
+    disableEnableBtn(true);
+  } else {
+    messageSel.textContent = "Your Turn";
+    disableEnableBtn(false);
+  }
+}
+
+// whenever button is clicked
+function makeMove() {
+  // if not my turn
+  if (!myTurn) return;
+
+  // if button is already having X or O
+  if (this.textContent.trim().length) {
+    return;
+  }
+
+  // emitting the symbol entered by player and btn id as obj
+  clicks ++ ;
+  socket.emit("make.move", {
+    symbol: symbol,
+    position: this.getAttribute("id"),
+  });
+}
+
+// return an obj which stores symbol in that box
+function getBoardState() {
+  let obj = {};
+  buttonSel.forEach((button) => {
+    let buttonId = button.getAttribute("id");
+    let buttonText = button.textContent || "";
+    obj[buttonId] = buttonText;
+  });
+  return obj;
+}
+
+socket.on("draw", () => {
+  resetSel.classList.remove("hide");
+  messageSel.style.height = "80vh";
+  messageSel.style.fontSize = "xx-large";
+  messageSel.style.color = "#FFA603";
+  messageSel.innerHTML = "Draw";
+  console.log("Draw is to be displayed");
+  no_draw = false;
+  disableEnableBtn(true);
+});
+
+function isGameOver() {
+  let state = getBoardState();
+  let matches = ["XXX", "OOO"];
+
+  // array storing state at all wining positions
+  let arr = [
+    state.a1 + state.a2 + state.a3,
+    state.b1 + state.b2 + state.b3,
+    state.c1 + state.c2 + state.c3,
+    state.a1 + state.b1 + state.c1,
+    state.a2 + state.b2 + state.c2,
+    state.a3 + state.b3 + state.c3,
+    state.a1 + state.b2 + state.c3,
+    state.a3 + state.b2 + state.c1,
+  ];
+
+  // if any of the wining condition is fulfilled => gameover
+  let is_gameOver = false;
+  arr.forEach((line) => {
+    if (line == matches[0] || line == matches[1]) {
+      console.log("game over");
+      is_gameOver = true;
+    }
+  });
+
+  // return the number of board fille with characters X or O
+  return is_gameOver;
+}
+
+// whenever move is made
+socket.on("move.made", (data) => {
+  document.getElementById(data.position).textContent = data.symbol;
+
+  // if the symbol of the last move was the same as the current player
+  // it means that now it's the opponent's turn
+  socket.emit("draw" , clicks) ;
+  myTurn = data.symbol !== symbol;
+  if (!isGameOver()) {
+    if (no_draw) displayMessage();
+  } else {
+    // else, showing win/lose message
+    //console.log(numberOfClicks) ;
+    resetSel.classList.remove("hide");
+    messageSel.style.height = "80vh";
+    messageSel.style.fontSize = "xx-large";
+
+    if (myTurn) {
+      console.log("changing heading");
+      messageSel.style.color = "#940303";
+      messageSel.innerHTML = "You lost.";
+    } else {
+      messageSel.style.color = "#07822c";
+      messageSel.innerHTML = "You won!";
+    }
+
+    disableEnableBtn(true);
+  }
+});
+
+// handling situation wher opp left
+socket.on("opponent.left", () => {
+  resetSel.classList.remove("hide");
+  messageSel.style.color = "white";
+  messageSel.textContent = "Your opponent left the game";
+  buttonSel.forEach((button) => {
+    button.disabled = true;
+  });
+});
+
+// reset button
+resetSel.addEventListener("click", () => {
+  location.reload();
+});
---
+++ server/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const http = require("http");
+const { Server } = require("socket.io");
+const server = http.createServer(app);
+const io = new Server(server);
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const port = process.env.PORT || 8888;
+// unmatched will store socket.id
+var players = {},
+  unmatched = null;
+
+// socket io
+io.on("connection", (socket) => {
+  console.log("New client conneted with user_socket.id: ", socket.id);
+
+  join(socket);
+
+  // assigns values to players obj, which contains details of opponent, symbol, socket
+  function join(socket) {
+    players[socket.id] = {
+      socket: socket,
+      opponent: unmatched,
+      symbol: "X",
+    };
+
+    // if player player is single
+    if (unmatched == null) {
+      unmatched = socket.id;
+    }
+
+    // if already one player exists in waiting room
+    else {
+      players[socket.id].symbol = "O";
+      players[unmatched].opponent = socket.id;
+      unmatched = null;
+    }
+  }
+
+  // returns opponent's socket
+  function opponentOf(socket) {
+    if (players[socket.id].opponent == null) return;
+    return players[players[socket.id].opponent].socket;
+  }
+
+  // if opponent exist emitting message to start the game
+  if (opponentOf(socket)) {
+    socket.emit("game.begin", {
+      symbol: players[socket.id].symbol,
+    });
+    opponentOf(socket).emit("game.begin", {
+      symbol: players[opponentOf(socket).id].symbol,
+    });
+  }
+
+  // event: (player move) listening
+  socket.on("make.move", (data) => {
+    // sending signal to both players about the made move
+    socket.emit("move.made", data);
+    opponentOf(socket).emit("move.made", data);
+
+    socket.on("draw" , (clicks) => {
+      opponentOf(socket).on("draw" , (oppClicks => {
+        if( clicks + oppClicks == 9 ) {
+          socket.emit("draw") ;
+        }
+      })) 
+    });
+  });
+
+  // client disconnetion
+  socket.on("disconnect", () => {
+    console.log("Disconnected Client socket.id: ", socket.id);
+    if (opponentOf(socket)) opponentOf(socket).emit("opponent.left");
+    socket.broadcast.emit("clientdisconnect", socket.id);
+  });
+});
+
+// static resources
+app.use(express.static(__dirname + "/../client/"));
+
+// home page
+app.get("/", (req, res) => {
+  const stream = fs.createReadStream(__dirname + "/../client/index.html");
+  stream.pipe(res);
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at ${port} port`);
+});
